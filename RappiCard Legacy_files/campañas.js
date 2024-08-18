bfeace60c6b89976fc91db9ed328585c7de4948c
Uzy <uzielmedina@hotmail.com>
2024-08-17 21:04:52 -0600

    Se agregó api-rewards y lo necesario para salir como beta

---
+++ RappiCard Legacy_files/campañas.js
@@ -0,0 +1,26 @@
+let campañas = {
+    "Gasolina": {
+      refund_Gasolina_digito: 2134,
+      post_Gasolina_digito: 2135,
+    },
+    "Farmacias": {
+      refund_1_digito: 2134,
+      post_1_digito: 336,
+      
+    },
+    "Uber": {
+      refund_1_digito: 2132,
+      post_1_digito: 2133,
+      
+    },
+    "Convenencia": {
+      refund_1_digito: 2130,
+      post_1_digito: 2131,
+      
+    },
+    "Supermercado": {
+      refund_1_digito: 2116,
+      post_1_digito: 2117,
+     
+    },
+  };
---
+++ campañas.js
@@ -0,0 +1,26 @@
+let campañas = {
+    "Gasolina": {
+      refund: 2134,
+      post: 2135,
+    },
+    "Farmacias": {
+      refund: 2134,
+      post: 2135,
+      
+    },
+    "Uber": {
+      refund: 2132,
+      post: 2133,
+      
+    },
+    "Convenencia": {
+      refund: 2130,
+      post: 2131,
+      
+    },
+    "Supermercado": {
+      refund: 2116,
+      post: 2117,
+     
+    },
+  };
